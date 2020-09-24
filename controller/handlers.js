a89018120e382612a090a81edc306e7821f7822d
William Duong <35279699+w-duong@users.noreply.github.com>
2020-09-24 11:21:50 -0700

    possible add-on for dstFolder argument

--- controller/handlers.js
+++ controller/handlers.js
@@ -4,18 +4,21 @@ const repo = require('./scratch');
 
 const create_repo = (fArray) => {
     srcDir = global.userInput[1];
+    dstDir = global.userInput[2];
 
     // IF '.JSTWepo' FOLDER DOESN'T EXIST, '.man' ALSO SHOULD NOT EXIST, THEREFORE
     // CREATE BOTH
-    if (!fs.existsSync(path.join(srcDir, '.JSTWepo'))) {
-        fs.mkdirSync(path.join(srcDir, '.JSTWepo'));
-        fs.mkdirSync(path.join(srcDir, '.JSTWepo', '.man'));
+    if (!fs.existsSync(path.join(dstDir, '.JSTWepo'))) {
+        fs.mkdirSync(path.join(dstDir, '.JSTWepo'));
+        fs.mkdirSync(path.join(dstDir, '.JSTWepo', '.man'));
     }
 
     // 'fileKeeper()' PARSES '{source path}' FOR ARCHIVABLE CONTENT
     // arg 1: 'fArray' will be populated with valid files
     repo.fileKeeper(srcDir, fArray);
 
+    // 'commitFiles()' COPIES VALID SOURCE FILES TO DESTINATION
+    // arg 1: 'fArray' contains absolute paths to files in 'srcDir'
     repo.commitFiles(fArray);
 
     // 'makeManifestFile()' GENERATES MANIFEST FILE AND NECESSARY ARTIFACT IDs
